ea46beacbf3d72d3ff92bfadbf7836386fd788aa
Rodrigo Magnavita <rodrigomagnavita@gmail.com>
2014-07-27 20:32:52 -0300

    evowave-ui: Recoding evowave logic
    1. Evowave Drag-Scroller capability in canvas

--- evowave-ui/src/main/js/modules/evowave/evowave.js
+++ evowave-ui/src/main/js/modules/evowave/evowave.js
@@ -2,7 +2,7 @@ angular.module( 'vismining-evowave', [] )
 
 	.run(['$templateCache', function($templateCache) {
 		$templateCache.put("template/vismining/evowave.html",
-			"<canvas style='border: 1px dashed #FFFFFF; padding:4px; background-color: #E4E4E4' />"
+			"<canvas style='border: 1px dashed #FFFFFF; padding:4px; background-color: #E4E4E4; outline: none; -webkit-tap-highlight-color: rgba(255, 255, 255, 0);' />"
 		);
 	}])
 
@@ -29,14 +29,45 @@ angular.module( 'vismining-evowave', [] )
 				sector_odd: 'FFF4F4F4',
 				sector_even:'FFEFEFEF',
 				guidelines: 'FF5796FD',
-				messages: 	'FF999999',
+				messages: 	'FFFFFFFF',
 				sectorlines:'FFFFFFFF'
 			};
 
-			this.init = function(gc) {
-				delete gc.draw;
+			this.init = function() {
 				
-				angular.extend(this, gc);
+				var sectorWithSmallestAngle;
+				var previousSector;
+
+				angular.forEach(this.data.sectors, function(sector, sectorId) {
+
+					var startAngle = 0;
+					var endAngle = (Math.PI * 2);
+
+					sector.startAngle = (previousSector === undefined) ? startAngle : previousSector.endAngle;
+					sector.endAngle = sector.startAngle + ((endAngle-startAngle) * sector.angle);
+
+					previousSector = sector;
+
+					if(sectorWithSmallestAngle === undefined){
+						sectorWithSmallestAngle = sector;
+					} else if(sectorWithSmallestAngle.angle > sector.angle){
+						sectorWithSmallestAngle = sector;
+					}
+
+				}, this);
+
+				this.smallestRadius = 0;
+
+				do {
+					this.smallestRadius++;
+					var offset = Math.atan( (this.data._window.size-2) / this.smallestRadius);
+				} while((sectorWithSmallestAngle.startAngle + offset) > (sectorWithSmallestAngle.endAngle - offset));
+
+				this.biggestRadius = (this.data._window.amount * this.data._window.size) + 8;
+
+				this.buffer = this.createGraphics((this.biggestRadius+1)*2, (this.biggestRadius+1)*2, 1);
+
+				this.center = {x: (this.width - this.buffer.width) / 2, y: ((this.height - this.buffer.height) / 2)};
 
 				this.reset();	
 				this.draw();
@@ -74,6 +105,9 @@ angular.module( 'vismining-evowave', [] )
 
 				this.dirty = {};
 
+				this.background(this.unhex(this.colors.background));
+				this.image(this.buffer, this.center.x, this.center.y, this.buffer.width, this.buffer.height);
+
 				console.log('Drawing in ' + (Date.now() - execTime) + 'ms');
 			};
 
@@ -83,27 +117,25 @@ angular.module( 'vismining-evowave', [] )
 
 				this.cleanSector(sectorId);
 
-				this.stroke(this.unhex(this.colors.sectorlines));
-				this.strokeWeight(2);
+				this.buffer.stroke(this.buffer.unhex(this.colors.sectorlines));
+				this.buffer.strokeWeight(2);
 
-				this.line(	(this.width/2) + (this.smallestRadius * this.cos(sector.startAngle)), 
-									(this.width/2) + (this.smallestRadius * this.sin(sector.startAngle)),
-									(this.width/2) + (this.biggestRadius * this.cos(sector.startAngle)), 
-									(this.width/2) + (this.biggestRadius * this.sin(sector.startAngle)));
+				this.buffer.line(	(this.buffer.width/2) + (this.smallestRadius * this.buffer.cos(sector.startAngle)), 
+									(this.buffer.width/2) + (this.smallestRadius * this.buffer.sin(sector.startAngle)),
+									(this.buffer.width/2) + (this.biggestRadius * this.buffer.cos(sector.startAngle)), 
+									(this.buffer.width/2) + (this.biggestRadius * this.buffer.sin(sector.startAngle)));
 
-				this.line(	(this.width/2) + (this.smallestRadius * this.cos(sector.endAngle)), 
-									(this.width/2) + (this.smallestRadius * this.sin(sector.endAngle)),
-									(this.width/2) + (this.biggestRadius * this.cos(sector.endAngle)), 
-									(this.width/2) + (this.biggestRadius * this.sin(sector.endAngle)));
-
-				console.log("$: " + (this.biggestRadius) + ", "+ (this.smallestRadius));
+				this.buffer.line(	(this.buffer.width/2) + (this.smallestRadius * this.buffer.cos(sector.endAngle)), 
+									(this.buffer.width/2) + (this.smallestRadius * this.buffer.sin(sector.endAngle)),
+									(this.buffer.width/2) + (this.biggestRadius * this.buffer.cos(sector.endAngle)), 
+									(this.buffer.width/2) + (this.biggestRadius * this.buffer.sin(sector.endAngle)));
 
 				for(var w = this.smallestRadius; w < (this.data._window.amount*this.data._window.size); w += this.data._window.size){
 					var startAngle = sector.startAngle + (Math.atan( 4 / w ));
 					var endAngle = sector.endAngle - (Math.atan( 4 / w ));
 
-					this.fill(255);
-					this.stroke(255);
+					this.buffer.fill(255);
+					this.buffer.stroke(255);
 					this.fillSector(startAngle, endAngle, (w+this.data._window.size)-2, w+2);
 				}
 
@@ -123,8 +155,8 @@ angular.module( 'vismining-evowave', [] )
 					sector.background = this.colors.sector_even;
 				}
 
-				this.fill(this.unhex(sector.background));
-				this.stroke(this.unhex(sector.background));
+				this.buffer.fill(this.buffer.unhex(sector.background));
+				this.buffer.stroke(this.buffer.unhex(sector.background));
 
 				var r = Math.random() * 255, g =  Math.random() * 255, b =  Math.random() * 255;
 
@@ -140,14 +172,32 @@ angular.module( 'vismining-evowave', [] )
 
 				while(angle <= endAngle){
 					var nextAngle = Math.min(endAngle, angle + (Math.PI/180));
-					this.beginShape();
-						this.vertex((this.width/2) + (biggestRadius * this.cos(angle)), (this.width/2) + (biggestRadius * this.sin(angle)));
-						this.vertex((this.width/2) + (biggestRadius * this.cos(nextAngle)), (this.width/2) + (biggestRadius * this.sin(nextAngle)));
-						this.vertex((this.width/2) + (smallestRadius * this.cos(nextAngle)), (this.width/2) + (smallestRadius * this.sin(nextAngle)));
-						this.vertex((this.width/2) + (smallestRadius * this.cos(angle)), (this.width/2) + (smallestRadius * this.sin(angle)));
-					this.endShape(2);
+					this.buffer.beginShape();
+						this.buffer.vertex((this.buffer.width/2) + (biggestRadius * this.buffer.cos(angle)), (this.buffer.width/2) + (biggestRadius * this.buffer.sin(angle)));
+						this.buffer.vertex((this.buffer.width/2) + (biggestRadius * this.buffer.cos(nextAngle)), (this.buffer.width/2) + (biggestRadius * this.buffer.sin(nextAngle)));
+						this.buffer.vertex((this.buffer.width/2) + (smallestRadius * this.buffer.cos(nextAngle)), (this.buffer.width/2) + (smallestRadius * this.buffer.sin(nextAngle)));
+						this.buffer.vertex((this.buffer.width/2) + (smallestRadius * this.buffer.cos(angle)), (this.buffer.width/2) + (smallestRadius * this.buffer.sin(angle)));
+					this.buffer.endShape(2);
 					angle += (Math.PI/180);
 				}
+			};
+
+			this.mouseDragged = function() {
+				this.externals.canvas.style.cursor = '-webkit-grabbing';
+				if(this.mouseTracker === undefined){
+					this.mouseTracker = {x: this.mouseX, y: this.mouseY};
+				} else {
+					this.center.x += this.mouseX - this.mouseTracker.x;
+					this.center.y += this.mouseY - this.mouseTracker.y;
+					this.draw();
+					this.mouseTracker.x = this.mouseX;
+					this.mouseTracker.y = this.mouseY;
+				}
+			};
+
+			this.mouseReleased = function() {
+				this.externals.canvas.style.cursor = 'default';
+				delete this.mouseTracker;
 			};
 
 			this.drawWindow = function(sectorId, windowId) {
@@ -160,67 +210,40 @@ angular.module( 'vismining-evowave', [] )
 
 			this.reset = function() {
 
-				var sectorWithSmallestAngle;
-				var previousSector;
-
-				angular.forEach(this.data.sectors, function(sector, sectorId) {
-
-					var startAngle = 0;
-					var endAngle = (Math.PI * 2);
-
-					sector.startAngle = (previousSector === undefined) ? startAngle : previousSector.endAngle;
-					sector.endAngle = sector.startAngle + ((endAngle-startAngle) * sector.angle);
-
-					previousSector = sector;
-
-					if(sectorWithSmallestAngle === undefined){
-						sectorWithSmallestAngle = sector;
-					} else if(sectorWithSmallestAngle.angle > sector.angle){
-						sectorWithSmallestAngle = sector;
-					}
-
-				}, this);
-
-				this.smallestRadius = 0;
-
-				do {
-					this.smallestRadius++;
-					var offset = Math.atan( (this.data._window.size-2) / this.smallestRadius);
-				} while((sectorWithSmallestAngle.startAngle + offset) > (sectorWithSmallestAngle.endAngle - offset));
-
-				this.biggestRadius = (this.data._window.amount * this.data._window.size) + 8;					
-
-				this.clean();
-				
-				this.noFill();
-				this.stroke(this.unhex(this.colors.guidelines));
-				this.strokeWeight(1);
-				this.arc((this.width/2), (this.width/2), (this.biggestRadius*2),  (this.biggestRadius*2), 0, Math.PI * 2);
-
-				this.biggestRadius -= 3;
-
-				this.noFill();
-				this.stroke(this.unhex(this.colors.guidelines));
-				this.strokeWeight(1);
-				this.arc((this.width/2), (this.width/2), (this.biggestRadius*2),  (this.biggestRadius*2), 0, Math.PI * 2);
-
 				if(this.data === undefined){
-					this.fill(this.unhex(this.colors.messages));
-					this.text(this.messages.noDataFound, ((this.biggestRadius*2) - this.textWidth(this.messages.noDataFound))/2, this.height/2);
+					this.buffer.fill(this.buffer.unhex(this.colors.messages));
+					this.buffer.text(this.messages.noDataFound, (this.buffer.width - this.textWidth(this.messages.noDataFound))/2, this.buffer.height/2);
 				} else {
-					this.noFill();
-					this.stroke(this.unhex(this.colors.guidelines));
-					this.strokeWeight(1);
-					this.arc((this.width/2), (this.width/2), (this.smallestRadius*2), (this.smallestRadius*2), 0, Math.PI * 2);
+
+					this.clean();
+					
+					this.buffer.noFill();
+					this.buffer.stroke(this.buffer.unhex(this.colors.guidelines));
+					this.buffer.strokeWeight(1);
+					this.buffer.arc((this.buffer.width/2), (this.buffer.width/2), (this.biggestRadius*2),  (this.biggestRadius*2), 0, Math.PI * 2);
+
+					this.biggestRadius -= 3;
+
+					this.buffer.noFill();
+					this.buffer.stroke(this.buffer.unhex(this.colors.guidelines));
+					this.buffer.strokeWeight(1);
+					this.buffer.arc((this.buffer.width/2), (this.buffer.width/2), (this.biggestRadius*2),  (this.biggestRadius*2), 0, Math.PI * 2);
+
+					this.buffer.noFill();
+					this.buffer.stroke(this.buffer.unhex(this.colors.guidelines));
+					this.buffer.strokeWeight(1);
+					this.buffer.arc((this.buffer.width/2), (this.buffer.width/2), (this.smallestRadius*2), (this.smallestRadius*2), 0, Math.PI * 2);
+
+					this.biggestRadius -= 4;
+					this.smallestRadius += 4;
 				}
 
-				this.biggestRadius -= 4;
-				this.smallestRadius += 4;
+				
 				
 			};
 
 			this.clean = function() {
-				this.background(this.unhex(this.colors.background));
+				this.buffer.background(this.buffer.unhex(this.colors.background));
 			}
 
 		};
@@ -235,29 +258,26 @@ angular.module( 'vismining-evowave', [] )
 			},
 			templateUrl: 'template/vismining/evowave.html',
 			link: function($scope, $element, attrs) {
-				$canvas = $element.find('canvas');
+				var $canvas = $element.find('canvas');
+				var canvas = $canvas[0];
 
 				evowave.data = $scope.data;
-				evowave.data = { _window: { size: 6, amount: 150 }, sectors: { sector5: { angle: 0.15 }, sector6: { angle: 0.35 }, sector3: { angle: 0.25 }, sector4: { angle: 0.05 }, sector2: { angle: 0.15 }, sector1: { angle: 0.05 } } };
+				evowave.data = { _window: { size: 6, amount: 100 }, sectors: { sector5: { angle: 0.15 }, sector6: { angle: 0.35 }, sector3: { angle: 0.25 }, sector4: { angle: 0.05 }, sector2: { angle: 0.15 }, sector1: { angle: 0.05 } } };
 
 				var size = parseInt(attrs.size);
 
-				new Processing($canvas[0], function(processing) {
-					var ProcessingAPIContext = processing; // TODO: Create a context wrapper object for processing
+				new Processing(canvas, function(processing) {
+					
+					angular.extend(processing, evowave);
 
 					processing.setup = function() {
-						/*if(evowave.data){
-							size = parseInt(parseInt(attrs.size)-(((((parseInt(attrs.size)/2)-10)-((((parseInt(attrs.size)/2) - 1) * 0.1)+11)) % evowave.data._window.size)*2));
-						}*/
-
 						processing.size(size, size);
-				  		evowave.init(ProcessingAPIContext);
 				  		processing.noLoop();
+				  		processing.init();
 					};
 
-					processing.draw = evowave.draw;
 				});
-				
+
 			}
 		};
 	}]);
