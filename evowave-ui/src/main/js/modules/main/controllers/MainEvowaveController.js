b72a34dd5c57cc385fae864512c71a0ce3ca8d28
Rodrigo Magnavita <rodrigomagnavita@gmail.com>
2014-08-05 13:44:58 -0300

    evowave-ui: Recoding evowave logic
    1. Changed odd-even colors
    2. Fixed float positions and sizes

--- evowave-ui/src/main/js/modules/main/controllers/MainEvowaveController.js
+++ evowave-ui/src/main/js/modules/main/controllers/MainEvowaveController.js
@@ -2,6 +2,14 @@ function MainEvowaveController( $scope ) {
 
 	$scope.screen = screen;
 
+	$scope.width = window.innerWidth;
+	$scope.height = window.innerHeight;
+
+	var size = 1024;
+
+	//$scope.width = 1586;
+	//$scope.height = 748;
+
 	$scope.jEdit = {
 		
 	};
