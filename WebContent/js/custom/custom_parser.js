664b82b73fe831889f0d1591a54af35617d0ed80
Hugo Deiró <hugodeiro@gmail.com>
2017-03-30 16:08:21 -0300

    Applying a custom example

--- WebContent/js/custom/custom_parser.js
+++ WebContent/js/custom/custom_parser.js
@@ -10,7 +10,141 @@ xhttp.onreadystatechange = function() {
         var responseText = xhttp.responseText;
         var responseObj = JSON.parse(responseText);
 
-        console.log(responseObj);
+        var data = {
+            period: {
+                starts: '',
+                ends: ''
+            },
+            query: '',
+            window: {
+                size: 16,
+                amount: 7,
+                mode: 'GLOBAL'
+            },
+            sector: {
+                label: 'project'
+            },
+            sectors: [
+                {
+                    angle: 0.4,
+                    label: 'Seção 1',
+                    windows: [
+                        {
+                            position: 1,
+                            molecules: [
+                                {
+                                    color: 'FFFF0000', 
+                                    data: {
+                                        complexity: 2,
+                                        LOC: 400
+                                    }
+                                },
+                                {
+                                    color: 'FF00FF00', 
+                                    data: {
+                                        complexity: 6,
+                                        LOC: 1500
+                                    }
+                                },
+                                {
+                                    color: 'FF0000FF', 
+                                    data: {
+                                        complexity: 5,
+                                        LOC: 900
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            position: 7,
+                            molecules: [
+                                {
+                                    color: 'FFFF0000', 
+                                    data: {
+                                        complexity: 3,
+                                        LOC: 440
+                                    }
+                                },
+                                {
+                                    color: 'FF00FF00', 
+                                    data: {
+                                        complexity: 5,
+                                        LOC: 300
+                                    }
+                                },
+                                {
+                                    color: 'FF0000FF', 
+                                    data: {
+                                        complexity: 4,
+                                        LOC: 550
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    angle: 0.6,
+                    label: 'Seção 2',
+                    windows: [
+                        {
+                            position: 1,
+                            molecules: [
+                                {
+                                    color: 'FFFF0000', 
+                                    data: {
+                                        complexity: 2,
+                                        LOC: 420
+                                    }
+                                },
+                                {
+                                    color: 'FF00FF00', 
+                                    data: {
+                                        complexity: 6,
+                                        LOC: 1300
+                                    }
+                                },
+                                {
+                                    color: 'FF0000FF', 
+                                    data: {
+                                        complexity: 5,
+                                        LOC: 940
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            position: 7,
+                            molecules: [
+                                {
+                                    color: 'FFFF0000', 
+                                    data: {
+                                        complexity: 3,
+                                        LOC: 460
+                                    }
+                                },
+                                {
+                                    color: 'FF00FF00', 
+                                    data: {
+                                        complexity: 5,
+                                        LOC: 360
+                                    }
+                                },
+                                {
+                                    color: 'FF0000FF', 
+                                    data: {
+                                        complexity: 4,
+                                        LOC: 580
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+
+        console.log(JSON.stringify(data));
         container.innerHTML = responseText;
     }
 }
